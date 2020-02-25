406d5b8e7f0c56237369e64eadbf96eae8d980a5
Jeremiah Dow <jeremiah.dow@pfhq.com>
2020-02-25 14:02:59 -0500

    03-controlled-vs-uncontrolled > exercise

--- courses/fundamentals/03-controlled-vs-uncontrolled/exercise/CheckoutBilling.js
+++ courses/fundamentals/03-controlled-vs-uncontrolled/exercise/CheckoutBilling.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react'
 import { MdShoppingCart } from 'react-icons/md'
-import serializeForm from 'form-serialize'
 import Heading from 'YesterTech/Heading'
 
 function CheckoutBilling({ onSubmit }) {
   const [sameAsBilling, setSameAsBilling] = useState(false)
+  const [billingName, setBillingName] = useState('')
+  const [shippingName, setShippingName] = useState('')
+  const [billingAddress, setBillingAddress] = useState('')
+  const [shippingAddress, setShippingAddress] = useState('')
 
   function handleSubmit(event) {
     event.preventDefault()
-    // When the fields are stored in state above, this fields variable can just be
-    // an object filled with the field states. We don't need `serializeForm` anymore
-    const fields = serializeForm(event.target, { hash: true })
+    const fields = {
+      billingName,
+      billingAddress,
+      shippingName: sameAsBilling ? billingName : shippingName,
+      shippingAddress: sameAsBilling ? billingAddress : shippingAddress
+    }
     onSubmit(sameAsBilling, fields)
   }
 
@@ -32,6 +38,11 @@ function CheckoutBilling({ onSubmit }) {
             required
             name="billingName"
             autoComplete="off"
+<<<<<<< HEAD
+=======
+            defaultValue={billingName}
+            onChange={e => setBillingName(e.target.value)}
+>>>>>>> 03-controlled-vs-uncontrolled > exercise
           />
         </div>
         <div className="form-field">
@@ -41,6 +52,11 @@ function CheckoutBilling({ onSubmit }) {
             type="text"
             required
             name="billingAddress"
+<<<<<<< HEAD
+=======
+            defaultValue={billingAddress}
+            onChange={e => setBillingAddress(e.target.value)}
+>>>>>>> 03-controlled-vs-uncontrolled > exercise
           />
         </div>
 
@@ -65,6 +81,12 @@ function CheckoutBilling({ onSubmit }) {
             required
             name="shippingName"
             autoComplete="off"
+<<<<<<< HEAD
+=======
+            value={sameAsBilling ? billingName : shippingName}
+            onChange={event => setShippingName(event.target.value)}
+            disabled={sameAsBilling}
+>>>>>>> 03-controlled-vs-uncontrolled > exercise
           />
         </div>
         <div className="form-field">
@@ -75,6 +97,9 @@ function CheckoutBilling({ onSubmit }) {
             required
             name="shippingAddress"
             autoComplete="off"
+            value={sameAsBilling ? billingAddress : shippingAddress}
+            onChange={event => setShippingAddress(event.target.value)}
+            disabled={sameAsBilling}
           />
         </div>
 
