23b919fe47ec26786bc132df944edaaea3a3fec0
Jeremiah Dow <jeremiah.dow@pfhq.com>
2020-02-25 13:37:23 -0500

    03-controlled-vs-uncontrolled > lecture

--- courses/fundamentals/03-controlled-vs-uncontrolled/lecture/Quantity.js
+++ courses/fundamentals/03-controlled-vs-uncontrolled/lecture/Quantity.js
@@ -3,13 +3,27 @@ import { FaMinusCircle, FaPlusCircle } from 'react-icons/fa'
 import 'YesterTech/Quantity.scss'
 
 function Quantity() {
-  const quantity = 1
+  const [quantity, setQuantity] = useState(1)
+
+  function subtract() {
+    setQuantity(quantity - 1)
+  }
+
+  function add() {
+    setQuantity(quantity + 1)
+  }
+
+  function onChange(e) {
+    const value = e.target.value
+    if (value.match(/[0-9]+/))
+      setQuantity(parseInt(value))
+  }
 
   return (
     <div className="quantity-picker">
       <div>
         <div>
-          <button type="button" className="icon-button">
+          <button type="button" className="icon-button" onClick={subtract}>
             <FaMinusCircle />
           </button>
         </div>
@@ -17,11 +31,16 @@ function Quantity() {
           <input
             type="text"
             aria-label="quantity"
+<<<<<<< HEAD
             defaultValue={quantity}
+=======
+            value={quantity}
+            onChange={onChange}
+>>>>>>> 03-controlled-vs-uncontrolled > lecture
           />
         </div>
         <div>
-          <button type="button" className="icon-button">
+          <button type="button" className="icon-button" onClick={add}>
             <FaPlusCircle />
           </button>
         </div>
