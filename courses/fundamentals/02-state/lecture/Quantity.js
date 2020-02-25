9e7d1789f9fe0e271cb86059134580927e01261c
Jeremiah Dow <jeremiah.dow@pfhq.com>
2020-02-25 11:31:14 -0500

    02-state > lecture

--- courses/fundamentals/02-state/lecture/Quantity.js
+++ courses/fundamentals/02-state/lecture/Quantity.js
@@ -3,23 +3,35 @@ import { FaMinusCircle, FaPlusCircle } from 'react-icons/fa'
 import 'YesterTech/Quantity.scss'
 
 export default function Quantity() {
-  const quantity = 1
+  const [quantity, setQuantity] = useState(1)
+  const [error, setError] = useState(null)
+
+  function subtract() {
+    if (quantity > 0) setQuantity(quantity - 1)
+    else setError('NO ')
+  }
+
+  function add() {
+    setQuantity(quantity + 1)
+    setError()
+  }
 
   return (
     <div className="quantity-picker">
       <div>
         <div>
-          <button type="button" className="icon-button">
+          <button type="button" className="icon-button" onClick={subtract}>
             <FaMinusCircle />
           </button>
         </div>
         <div className="input-container">{quantity}</div>
         <div>
-          <button type="button" className="icon-button">
+          <button type="button" className="icon-button" onClick={add}>
             <FaPlusCircle />
           </button>
         </div>
       </div>
+      {error && <span style={{ color: 'red', fontSize: 17 }}>{error}</span>}
     </div>
   )
 }
