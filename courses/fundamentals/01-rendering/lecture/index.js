29cc30910a061df05ae7566029656c71f91d19d2
Jeremiah Dow <jeremiah.dow@pfhq.com>
2020-02-25 11:29:06 -0500

    01-rendering > lecture

--- courses/fundamentals/01-rendering/lecture/index.js
+++ courses/fundamentals/01-rendering/lecture/index.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import ReactDOM from 'react-dom'
 import { FaRegStar, FaStar } from 'react-icons/fa'
 import 'YesterTech/styles/global-styles.scss'
 import './styles.scss'
 
-// let's go
+// React is:
+// 1. Composable
+// 2. Declarative
+
+function Button({ children, onEnter }) {
+  return (
+    <button
+      onKeyDown={e => {
+        if (e.key === 'Enter') {
+          onEnter()
+        }
+      }}
+      className="render_button"
+      style={{ fontWeight: 700, display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+    >
+      {children}
+    </button>
+  )
+}
+
+let domElement = document.querySelector('#root')
+
+ReactDOM.render(
+  <div>
+    <Button onEnter={() => console.log('Enter')}>
+      <FaStar /> Tacos
+    </Button>
+    <Button>
+      <FaRegStar /> Pizza
+    </Button>
+  </div>,
+  domElement
+)
