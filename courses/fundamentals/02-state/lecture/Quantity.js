5e33355b651f80821d5206a94519916479477a54
Jeremiah Dow <jeremiah.dow@pfhq.com>
2020-02-26 09:16:50 -0500

    roll our own useState hook

--- courses/fundamentals/02-state/lecture/Quantity.js
+++ courses/fundamentals/02-state/lecture/Quantity.js
@@ -1,6 +1,29 @@
-import React, { useState } from 'react'
-import { FaMinusCircle, FaPlusCircle } from 'react-icons/fa'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { FaMinusCircle, FaPlusCircle, FaSatelliteDish } from 'react-icons/fa'
 import 'YesterTech/Quantity.scss'
+import { doc } from 'prettier'
+
+const states = []
+let callCount = -1
+
+function useState(initialValue) {
+  const id = ++callCount
+
+  if (id < states.length) {
+    return states[id]
+  }
+
+  function setValue(value) {
+    states[id][0] = value
+    console.log(states)
+    renderPhonyHooks()
+  }
+
+  const tuple = [initialValue, setValue]
+  states.push(tuple)
+  return tuple
+}
 
 export default function Quantity() {
   const [quantity, setQuantity] = useState(1)
@@ -35,3 +58,13 @@ export default function Quantity() {
     </div>
   )
 }
+
+function renderPhonyHooks() {
+  callCount = -1
+  ReactDOM.render(
+    <Quantity />,
+    document.getElementById('root')
+  )
+}
+
+renderPhonyHooks()
